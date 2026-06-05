f83eb8637bdd70874ccab3247f1ff87123b20750
Captain Jack Sparrow <15pwele4721@uetpeshawar.edu.pk>
2026-06-05 11:14:22 +0500

    using tavily search as tool

--- main.py
+++ main.py
@@ -4,26 +4,16 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 import os
 
 load_dotenv()
 
-tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+tavily_search = TavilySearch(api_key=os.getenv("TAVILY_API_KEY"))
 
-@tool
-def search_web(query: str) -> str:
-    """Tool to search the web for information.
-    Args:
-        query: The query to search the web for.
-    Returns:
-        The search results.
-    """
-    print(f"Searching the web for {query}")
-    return tavily_client.search(query)
 
 llm = ChatOpenAI(model="gpt-5-nano")
-tools = [search_web]
+tools = [tavily_search]
 agent = create_agent(model=llm, tools=tools)
 
 def main():
