738beb937e61613433b3ee046955dcf228868fda
Captain Jack Sparrow <15pwele4721@uetpeshawar.edu.pk>
2026-06-05 11:46:14 +0500

    structured ouput using pydantic

--- main.py
+++ main.py
@@ -6,15 +6,25 @@ from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
 import os
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
 tavily_search = TavilySearch(api_key=os.getenv("TAVILY_API_KEY"))
 
+class Source(BaseModel):
+    """Information about the source"""
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Response from the agent"""
+    answer: str = Field(description="The answer to the question")
+    sources: list[Source] = Field(default_factory=list, description="The sources used to answer the question")
+
 
 llm = ChatOpenAI(model="gpt-5-nano")
 tools = [tavily_search]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from search-engine!")
